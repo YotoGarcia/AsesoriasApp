9ea37700abf0063b4b674699977a3e53768e0283
Jonathan Garcia <jonathan.garciaab@est.iudigital.edu.co>
2024-11-03 20:03:12 -0500

    ultimo commit

--- index.js
+++ index.js
@@ -16,12 +16,6 @@ connectDB();
 app.use(cors()); 
 app.use(express.json()); 
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
-
-
 
 app.use('/cliente', require('./src/routes/clienteRoute')); 
 app.use('/etapa', require('./src/routes/etapaRoute.js'));
